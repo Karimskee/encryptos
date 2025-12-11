b049aa508ec22e9c6ddfb4cbed660c642c0fbe6d
Karimskee <kareemgamer2006@gmail.com>
2025-12-11 08:56:04 +0200

    feat: monoalphabetic logic implementation
    - monoalphabetic class & node
    - smart key selector code
    - - making sure required key type is selected for each cipher algorithm
    - - currently supports:
    - - - integer key (caesar)
    - - - alphabet permutation string key (monoalphabetic)

--- Scripts/ciphers/test_ciphers.gd
+++ Scripts/ciphers/test_ciphers.gd
@@ -3,31 +3,45 @@ extends Node
 
 # Global variables
 var plain_text = "Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz 1234567890 !@#$%^&*()_+-=~,./;:'\"[]{}<>"
-var key = 3
+var key_int = 3
+var key_string = "BACDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 func _ready():
 	print("[TESTING CIPHERS]\n")
 	
 	# ADD NEW CIPHERS HERE
-	var ciphers = [Caesar]
+	var ciphers = [
+		Caesar,
+		Monoalphabetic
+	]
 	
 	for cipher in ciphers:
-		test(cipher, plain_text, key)
+		test(cipher, plain_text)
 
 
-func test(cipher: Script, plain_text: String, key: Variant) -> void:
+func test(cipher: Script, plain_text: String) -> void:
+	var key
+	var methods = cipher.get_script_method_list()
+	
+	# Different cipher need different key types
+	# The code below adapts to the required cipher type
+	if methods[0].args[0].type == 2:	# TYPE_INT == 2
+		key = key_int
+	else:								# TYPE_STRING == 4 or VARIANT == 0
+		key = key_string
+	
 	# For debugging
 	print(cipher.get_global_name() + " cipher")
 	print("Key: ", key)
 	print("Plain Text: ", plain_text)
 	
 	# Encrypt
-	var cipher_text = cipher.encrypt(plain_text, key)
-	print("Encrypted:  ", cipher_text)
+	var cipher_text = cipher.encrypt(key, plain_text)
+	print("Encrypted:  ", cipher_text) 
 	
 	# Decypt
-	var decrypted_text = cipher.decrypt(cipher_text, key)
+	var decrypted_text = cipher.decrypt(key, cipher_text)
 	print("Decrypted:  ", decrypted_text)
 	
 	if (plain_text == decrypted_text):
