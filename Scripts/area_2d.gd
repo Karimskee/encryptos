67d3c8eda68eda5f87e3f05443068b62f3f6c6b1
Abdelrahman ZaZou <abdelrahman.za3zo3@gmail.com>
2025-12-14 22:40:03 +0200

    Redesign ui && Add Transition

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -22,6 +22,6 @@ func _process(delta: float) -> void:
 	if entered == true:
 		if Input.is_action_just_pressed("Enter"):
 			print("Enter pressed! Changing scene...")
-			get_tree().change_scene_to_file("res://Scens/encryption.tscn")  
+			Transition.fade_out(func ():get_tree().change_scene_to_file("res://Scens/encryption.tscn"))  
 			
 			
