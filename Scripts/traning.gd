488936cecdfe1d4a7d56decd3cb5e68e6c9e035d
Abdelrahman ZaZou <abdelrahman.za3zo3@gmail.com>
2025-12-16 18:00:21 +0200

    final-boss

--- Scripts/traning.gd
+++ Scripts/traning.gd
@@ -8,4 +8,4 @@ func _ready():
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and not changed:
 		changed = true
-		Transition.fade_out(func ():get_tree().change_scene_to_file("res://Scens/level_2.tscn"))
+		Transition.fade_out(func ():get_tree().change_scene_to_file("res://Scens/encryption.tscn"))
